7b3f843922c49e5c7b64a689de5cbd27d213e773
Zeerak Talat <z@zeerak.org>
2024-07-17 01:43:19 +0400

    Change papers to be a dictionary and clean up fetching code.

--- parse_results.py
+++ parse_results.py
@@ -12,44 +12,51 @@ def search_s2(
     query: list,
     fields: list,
     year: int = 1987,
-    paper_count: int = 20000,
+    max_results: int = 20000,
     venue: str = "Interspeech",
-    downlad: bool = True,
+    **kwargs,
 ):
-    fields = ','.join(fields) if len(fields) > 1 else fields
-    query = ','.join(query) if len(query) > 1 else query
-    url = f"http://api.semanticscholar.org/graph/v1/paper/search/bulk?query={query}&fields={','.join(fields)}&year={year}-&venue={venue}&limit={paper_count}"
+    """Search Semantic scholar for papers."""
+
+    # Ensure ExternalIds (contain DOI) is always the last element.
+    if 'externalIds' not in fields:
+        fields.append('externalIds')
+    elif 'ExternalIds' in fields:
+        fields.pop('ExternalIds')
+    fields.append('ExternalIds')
+
+    fields = ",".join(fields) if len(fields) > 1 else fields
+    query = ",".join(query) if len(query) > 1 else query
+    url = f"http://api.semanticscholar.org/graph/v1/paper/search/bulk?query={query}&fields={','.join(fields)}&year={year}-&venue={venue}&limit={max_results}"
     r = requests.get(url).json()
+    collected = 0
+
     print(f"Will retrieve an estimated {r['total']} documents")
 
-    with open("papers.csv", "a") as tsv_file, open("err.txt", "a") as err:
-        err_count = 0
-        collected = 0
-        err_writer = csv.writer(err, delimeter="\t")
-        info_writer = csv.writer(tsv_file, delimiter="\t")
+    while True:
+        if "data" in r:
+            for paper in tqdm(r["data"]):
+                res = {key: paper.get(key) for key in fields}
+                collected += 1
+                yield res
 
-        while True:
-            if "data" in r:
-                for paper in tqdm(r["data"]):
-                    res = [paper.get(key) for key in fields]
-                    url = extract_url(paper)
-                    if url:
-                        collected += 1
-                        url = parse_url(url)
-                        res.append(url)
-                        info_writer.write(res)
+        if "token" not in r:
+            break
+    r = requests.get(f"{url}&token={r['token']}").json()
+    print(f"Done!\n{collected} papers retrieved.")
 
-                        if download:
-                            download_pdf(url, pdf_dir)
 
-                    else:
-                        err += 1
-                        err_writer.write(paper["title"] + "\n")
-                        continue
-            if "token" not in r:
-                break
-        r = requests.get(f"{url}&token={r['token']}").json()
-    print(f"Done!\n{collected} papers retrieved.\n{err_count} links not available.")
+def extract_doi(paper):
+    try:
+        return paper["externalIds"]["DOI"]
+    except KeyError:
+        return False
+
+
+def extract_url(doi):
+    """Use the DOI to get the actual URL for the publication."""
+    resp = pydoi.get_url(doi)
+    return resp
 
 
 def parse_url(url):
@@ -67,36 +74,13 @@ def download_pdf(url, out_dir):
     f_name = url.split("/")[-1]
     path = f"{out_dir}/{f_name}"
 
-    resp = requests.get(url)
-    with open(path, "wb") as file:
-        file.write(resp.content)
-    return 1
-
-
-def download_pdfs(in_file, s2results, out_dir):
-    with open(in_file, "r", encoding="utf-8") as urls, open(
-        "dois.txt", "r", encoding="utf-8"
-    ) as info, open("download.err", "a", encoding="utf-8") as err, open(
-        "download.txt", "w", encoding="utf-8"
-    ) as out_f:
-        info_dict = {}
-        counter = 0
-        dl_counter = 0
-        err_counter = 0
-
-        for line in info:
-            line = line.strip().split(",")
-            info_dict[line[0]] = {item for item in line[1:]}
-
-        for line in tqdm(urls, total=19230):
-            try:
-                doi, url = line.strip().split(",")
-                parsed_url = parse_url(url)
-                f_name = url.split("/")[-1]
-                path = f"papers/{f_name}"
-
-                # Download every paper
-                counter += download_pdf(parsed_url)
+    try:
+        resp = requests.get(url)
+        with open(path, "wb") as file:
+            file.write(resp.content)
+        return True
+    except Exception:
+        return False
 
 
 def write_csv(info_dict):
@@ -135,6 +119,7 @@ def write_csv(info_dict):
 
 
 if __name__ == "__main__":
+    pass
     # 0. Pull papers
     # get_references("")
     # 1. Extract DOIs
@@ -145,8 +130,3 @@ if __name__ == "__main__":
     # download_pdfs("urls.txt", None, "papers/")
     # 4. Run the pdf to text extraction script on the commandline.
     # 5. Extract all papers with a particular pattern.
-    info_dict = search_files(
-        ["emotion", "emo-", "paralinguistic", "para-linguistic", "valence", "arousal"]
-    )
-    # 6. Write CSV with all information presented.
-    write_csv(info_dict)
